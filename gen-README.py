53cda67c194c33255f18522e4b0ab406a2786767
dk949 <56653556+dk949@users.noreply.github.com>
2021-07-28 22:20:35 +0300

    generator v2.1

--- gen-README.py
+++ gen-README.py
@@ -1,12 +1,5 @@
-def processBlock(block: list):
-    out = ""
-    out += f"<a {block[0][0]} target=\"_blank\"><img {block[0][1]} {block[0][2]} {block[0][3]}/></a>\n"
-    if(len(block) > 1):
-        out += "&nbsp;&nbsp;&nbsp; &nbsp; &nbsp;\n"
-        for fw in block[1]:
-            out += f"<a {fw[0]} target=\"_blank\"><img {fw[1]} {fw[0][2]} {fw[3]}/></a>\n"
-    out +='\n'
-    return out
+def processBlock(l: list):
+    return ''.join([f"<a {l[0][0]} target=\"_blank\"><img {l[0][1]} {l[0][2]} {l[0][3]}/></a>\n",''.join(''.join(["&nbsp;&nbsp;&nbsp; &nbsp; &nbsp;\n", ''.join([f"<a {m[0]} target=\"_blank\"><img {m[1]} {m[0][2]} {m[3]}/></a>\n" for m in l[1]])])if len(l) > 1 else [])]) + '\n'
 
 
 open("README.md", "w").write(open("template-intro.md", "r").read() + ''.join([processBlock(l) for l in [[[k for k in j.split('\n') if k != ""] if c == 0 else [k.split('\n') for k in j.split("-"*32 + '\n') if k != ""] for c, j in enumerate(i.split("-" * 32 + '\n' + "-" * 32 + '\n'))] for i in open("frameworks", "r").read().split("\n\n\n")]]) + open("template-outro.md", "r").read())
